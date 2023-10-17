997f838c72488d8a84e841101eaa7c84a78d62f3
ErfanMaster <erfan33200@gmail.com>
2023-10-17 18:48:18 +0330

    composition of cin with while and using ignore and clear

--- Project.cpp
+++ Project.cpp
@@ -1,13 +1,16 @@
 #include<iostream>
+#include <limits>
 using namespace std;
 int main()
 {
 int age;
 cout<<"Please Enter Your Age : ";
-while(cin>>age)
+while(!(cin>>age))
     {
-        cout<<"\n Your Age Is :" << " " << age;
+        cout<<"Please Enter a Number :";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
     }
-
+cout<<"Your Age Is : "<<age;
 return 0;
 }
