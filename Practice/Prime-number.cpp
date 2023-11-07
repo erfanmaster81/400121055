97c46e5e02174e0e65509b39def7411fcee74050
ErfanMaster <erfan33200@gmail.com>
2023-11-08 00:32:02 +0330

    practice1

--- Practice/Prime-number.cpp
+++ Practice/Prime-number.cpp
@@ -5,7 +5,7 @@ int main()
 {
 Math math;
  int number;
- cout<<"Enter a Number To Print Prime Number forom 1 to Number:";
+ cout<<"Enter a Number To Print Prime Number from 1 to Number:";
  cin>>number;
  for(int i=0;i<number;i++)
     if(math.isPrime(i))
--- Practice/math.h
+++ Practice/math.h
@@ -6,11 +6,15 @@ class Math
              if (number <= 1) {
              return false;
              }
-            for (int i = 2; i * i <= number; i++) {
-                   if (number % i == 0) {
-                          return false;
+             int cnt=2;
+            while(cnt<number)
+            {
+                if(number % cnt == 0)
+                    {
+                        return false;
                     }
-                 }
+                    cnt++;
+            }
 
          return true;
         }
